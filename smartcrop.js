9c914679f9242e9dd33c4067487e7425dab64a4f
Eduardo Bouças <mail@eduardoboucas.com>
2016-07-04 09:54:29 +0100

    Add options object to iop.open call

--- smartcrop.js
+++ smartcrop.js
@@ -88,7 +88,7 @@ smartcrop.crop = function(inputImage, options_, callback) {
   var scale = 1;
   var prescale = 1;
 
-  return iop.open(inputImage).then(function(image) {
+  return iop.open(inputImage, options).then(function(image) {
 
     if (options.width && options.height) {
       scale = min(image.width / options.width, image.height / options.height);
