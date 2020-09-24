7e8408c743aa0b512c0c52b998eb09c9ef369bf1
Alexander Gusman <alex@goooseman.ru>
2020-09-24 19:31:11 +0300

    Adds Typescript definitions (#100)
    * Add typescript definitions
    
    * Change images types to CanvasImageSource in ts typings

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+export interface Crop {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+export interface CropResult {
+  topCrop: Crop;
+}
+export interface CropBoost {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  weight: number;
+}
+export interface CropOptions {
+  minScale?: number;
+  width: number;
+  height: number;
+  boost?: CropBoost[];
+  ruleOfThirds?: boolean;
+  debug?: boolean;
+}
+export const smartcrop: {
+  crop(image: CanvasImageSource, options: CropOptions): Promise<CropResult>;
+};
+export default smartcrop;
